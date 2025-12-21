9d0674a3b7c3f860defd96e2b1df4204d49e6af9
ellailan <pixelpoodle@gmail.coml>
2025-12-20 16:36:03 -0800

    bar graphs for student status, faculties

--- summarygraphs.R
+++ summarygraphs.R
@@ -1,5 +1,50 @@
 source("datacleaning.R")
-#only drop NAs when needed... 
+
+student_status <- df |> select(student_status) |> drop_na()
+
+
+student_status_dist <- df |>
+  select(student_status) |>
+  drop_na() |>
+  ggplot(aes(x = student_status, fill = student_status)) +
+  geom_bar(alpha = 0.9) +
+  scale_fill_manual(
+    values = wesanderson::wes_palette(
+      "Royal1",
+      n = length(unique(df$student_status)),
+      type = "continuous"
+    )
+  ) +
+  labs(
+    x = "Student Status",
+    y = "Number of Respondents"
+  ) +
+  theme_minimal(base_size = 13) +
+  theme(
+    axis.title = element_text(face = "bold"),
+    axis.text.x = element_text(angle = 30, hjust = 1),
+    panel.grid.major.x = element_blank(),
+    legend.position = "none"
+  )
+
+student_status_dist
+
+#some numbers leftover from early survey design, testing... cutting them out here
+faculty <- df |> select(student_society) |> drop_na() |> filter(!grepl("[0-9]", student_society))
+faculty
+
+faculty_dist <- ggplot(faculty, aes(x = fct_infreq(student_society), fill = student_society)) +
+  geom_bar(show.legend = FALSE) +
+  coord_flip() +
+  labs(
+    title = "Student Society Memberships",
+    x = "Student Society",
+    y = "Count"
+  ) +
+  theme_minimal(base_size = 14)
+
+faculty_dist
+
 commute <- df |> select(commute_time) |> drop_na() |> 
   mutate(commute_time = as.numeric(commute_time)) |>
   filter(commute_time <= 480) #some troll values screwed up the graph
